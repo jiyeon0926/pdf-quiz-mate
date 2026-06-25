b79bce99be2ee8586dd6bf9fa09a252e07493db8
jiyeon0926 <yeoni9094@naver.com>
2026-06-25 17:46:38 +0900

    feat: 문제 생성 로딩 화면 구현

--- src/main/java/jy/quiz/controller/QuizController.java
+++ src/main/java/jy/quiz/controller/QuizController.java
@@ -4,25 +4,25 @@ import jy.quiz.enums.QuestionType;
 import jy.quiz.service.QuizService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
 @Controller
+@RequestMapping("/quiz")
 @RequiredArgsConstructor
 public class QuizController {
 
     private final QuizService quizService;
 
-    @GetMapping("/quiz")
+    @GetMapping
     public String generateForm() {
         return "generate";
     }
 
-    @PostMapping("/quiz")
+    @PostMapping
     public String generateQuiz(@RequestParam QuestionType questionType,
                                @RequestParam int questionCount,
                                @RequestParam MultipartFile file) {
@@ -32,6 +32,13 @@ public class QuizController {
                 file
         );
 
-        return "redirect:/quiz/" + uuid;
+        return "redirect:/quiz/" + uuid + "/loading";
+    }
+
+    @GetMapping("/{uuid}/loading")
+    public String showLoading(@PathVariable UUID uuid, Model model) {
+        model.addAttribute("uuid", uuid);
+
+        return "loading";
     }
 }
